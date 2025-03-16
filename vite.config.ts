087a29c9edd36b0f3a99c98c018f9c96bd2bee95
ewsgit <ewsgit@duck.com>
2025-03-16 18:21:37 +0000

    Initial commit

---
+++ vite.config.ts
@@ -0,0 +1,44 @@
+/*
+ * Copyright ©2025 Ewsgit<https://ewsgit.uk> and YourDash<https://yourdash.ewsgit.uk> contributors.
+ * YourDash is licensed under the MIT License. (https://mit.ewsgit.uk)
+ */
+
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+import dtsPlugin from "vite-plugin-dts";
+import dynamicImport from "vite-plugin-dynamic-import";
+import { libInjectCss } from "vite-plugin-lib-inject-css";
+import { resolve } from "path";
+
+// ViteJS docs: https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    react({
+      babel: {
+        plugins: [["babel-plugin-react-compiler", {}]],
+      },
+    }),
+    // @ts-ignore
+    dynamicImport(),
+    libInjectCss(),
+    dtsPlugin(),
+  ],
+  build: {
+    lib: {
+      entry: resolve(__dirname, "src/index.tsx"),
+      formats: ["es"],
+    },
+    sourcemap: false,
+    minify: true,
+    rollupOptions: {
+      external: ["react", "react/jsx-runtime", "react-router", "react-router-dom"],
+    },
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        api: "modern-compiler", // or "modern"
+      },
+    },
+  },
+});
